8090807a8366f944c040d263318844819e10cd0d
rosh385k <152947720+rosh385k@users.noreply.github.com>
2023-12-05 12:52:42 -0600

    Add files via upload

---
+++ Lab 4 Program 1.cpp
@@ -0,0 +1,35 @@
+//TO-DO: Fill in the Authors Name Here
+//TO-DO: Fill in the current date here
+//CS1428 Lab
+//Lab 4
+//Description: this program will calculate the cost of a TV
+//    plan based on user choices.
+
+//****************This is the line of 80 characters in length*******************
+//############Your code should not exceed the length of the above line##########
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+    char user_choice;  //user's choice
+    double price; //price of monthly package
+
+    // Ask the user for their plan choice:
+    cout << "Enter the package name you wish to purchase: ";
+    cin >> user_choice;
+
+    // TODO: Use a switch statement to evaluate the total cost of the
+    // customer's monthly bill based on the selected plan.
+
+
+
+
+    cout << "The total price of your monthly package is $" << price << endl;
+
+    // Exit the program:
+    return 0;
+}
